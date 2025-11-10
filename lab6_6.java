dd6979c5a55d45d932d6e587c39a0fda553468fe
scaredofthesix <bodulevmax@gmail.com>
2025-11-10 23:25:05 +0300

    Create lab6_6.java

---
+++ lab6_6.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class lab6_6{
+    public static void main(String[] arg){
+        Scanner s = new Scanner(System.in);
+        double x = s.nextDouble();
+        System.out.println((x - 32) * 5/9);
+
+    }
+}
