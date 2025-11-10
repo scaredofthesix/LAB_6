ced17c179aadadbd67941e046d81e2c5822906f2
scaredofthesix <bodulevmax@gmail.com>
2025-11-10 23:03:11 +0300

    lab6_3.java

---
+++ lab6_3.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class lab6_3{
+    public static void main(String[] arg){
+        for(char ch = 'A'; ch<= 'Z';ch++){
+            System.out.println(ch - 'A' + 1 + " " + ch);
+        }
+    }
+}
